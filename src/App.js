73ffee631ec7cb3451963763e659d4afb8c06898
Vineet Chotaliya <vineetchotaliya30@gmail.com>
2023-02-23 18:51:56 +0530

    Implementing Login and Signin Screen

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
 import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import { auth } from './firebase';
 
 function App() {
+  const user = null;
+
+  useEffect(()=>{
+    const unsubscribe = auth.onAuthStateChanged(userAuth =>{
+      if(userAuth){
+        //Logged in
+        console.log(userAuth)
+      }
+      else{
+        //Logged out
+      }
+    });
+    return unsubscribe;
+  },[])
+  
   return (
     <div className="App">
       <Router>
-        <Routes>
-          <Route exact path="/" element={<HomeScreen/>}/>
-        </Routes>
+        {!user ? (
+          <LoginScreen/>
+        ):(
+          <Routes>
+            <Route exact path="/" element={<HomeScreen/>}/>
+          </Routes>
+        )}
       </Router>
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,8 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyAbtdEyv9nJewGQHbsrHTxuzbqb6fYsHpw",
     authDomain: "netflix-clone-a7ed4.firebaseapp.com",
@@ -6,3 +11,13 @@ const firebaseConfig = {
     messagingSenderId: "737874571367",
     appId: "1:737874571367:web:eee08280a10f9e34c664c2"
   };
+
+  // initializing the firebase app 
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  // firestore db will allow us to keep a track of the user subscription
+  const db= firebaseApp.firestore();
+  // for authnetication
+  const auth = firebase.auth();
+
+  export {auth};
+  export default db;
